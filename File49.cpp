deb98ca42ea30e519ddd144b24f7d02aa3bc82ba
MetaMsa <90111767+MetaMsa@users.noreply.github.com>
2023-12-26 22:05:40 +0300

    Update File49.cpp

--- File49.cpp
+++ File49.cpp
@@ -11,42 +11,22 @@ int _tmain(int argc, _TCHAR* argv[])
  cin>>number1;
  cout<<"Please write a number";
  cin>>number2;
- if(number1==0 || number2==0)
- {
-	 gcd=0;
- }
+ if(number1==0 || number2==0) gcd=0;
  else if(number2>number1)
  {
-	 if(number2%number1==0)
-		gcd=number1;
+	 if(number2%number1==0)gcd=number1;
 	 else
-	 {
 	 for (int i=2;i<=number2;i++)
-	 {
-	  if(number1%i==0&&number2%i==0)
-	  {
-	   gcd=i;
-	  }
-	 }
-	 }
+	  if(number1%i==0&&number2%i==0) gcd=i;
  }                          
  else if(number1>number2)
  {
-	 if(number1%number2==0)
-		 gcd=number2;
+	 if(number1%number2==0) gcd=number2;
 	 else
-	 {
 	   for (int j=2;j<=number1;j++)
-	 {
-	  if(number1%j==0&&number2%j==0)
-	  {   
-	   gcd=j;
-	  }
-	 }
-	 }
+	  if(number1%j==0&&number2%j==0) gcd=j;
  }
- else if(number1==number2)
-	 gcd=number1;
+ else if(number1==number2) gcd=number1;
  cout<<"gcd of numbers = "<<gcd;
  bitir
  bitir
